b761d69ac7a7ded73a69658d26b8a35e5f418009
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-13 16:30:41 -0500

    completed exercise 10

--- estructuras-de-control/estructura-secuencial/ejercicio_10.java
+++ estructuras-de-control/estructura-secuencial/ejercicio_10.java
@@ -5,6 +5,34 @@
  * deben quedar así: 3544 y el otro 652.
  */
 
+import java.util.Scanner;
+
 public class ejercicio_10 {
 
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int primerNumero = 0;
+    int segundoNumero = 0;
+    int auxiliar = 0;
+    int auxiliarDos = 0;
+
+    System.out.print("\nIngrese primer numero = ");
+    primerNumero = sc.nextInt();
+
+    System.out.print("Ingrese segundo numero = ");
+    segundoNumero = sc.nextInt();
+
+    auxiliar = primerNumero % 10;
+    auxiliarDos = segundoNumero % 10;
+
+    primerNumero = (primerNumero - auxiliar) + auxiliarDos;
+    segundoNumero = (segundoNumero - auxiliarDos) + auxiliar;
+
+    System.out.println("\n1. Primer número = " + primerNumero);
+    System.out.println("2. Segundo número = " + segundoNumero);
+
+    sc.close();
+  }
 }
