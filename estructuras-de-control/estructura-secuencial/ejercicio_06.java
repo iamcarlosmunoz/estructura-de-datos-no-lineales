a05136018ec004291d3a75a3394f55f6a0e6ef7e
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-13 14:48:50 -0500

    add exercise 06

---
+++ estructuras-de-control/estructura-secuencial/ejercicio_06.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ejercicio_06 {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    float primerValorFahrenheit = 0f;
+    float segundoValorCentrigrados = 0f;
+
+    float gradosFahrenheit = 0f;
+    float gradosCentrigrados = 0f;
+
+    System.out.print("\nIngrese PRIMER valor en Fahrenheit = ");
+    primerValorFahrenheit = sc.nextFloat();
+
+    System.out.print("Ingrese SEGUNDO valor en Centígrados = ");
+    segundoValorCentrigrados = sc.nextFloat();
+
+    gradosFahrenheit = (9f / (5f * segundoValorCentrigrados)) + 32;
+    gradosCentrigrados = 9f / (5f * (primerValorFahrenheit - 32));
+
+    System.out.println("\n1. " + primerValorFahrenheit + " Fahrenheit a Centígrados = " + gradosCentrigrados);
+    System.out.println("2. " + segundoValorCentrigrados + " Centígrados a Fahrenheit = " + gradosFahrenheit);
+
+    sc.close();
+
+  }
+}
