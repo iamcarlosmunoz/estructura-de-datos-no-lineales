ee1d33d87c0aef78fd1bc6f5494fa93ac4f25308
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-18 21:55:13 -0500

    completed exercise_05 es

--- estructuras-de-control/estructuras-selectivas/ejercicio_05.java
+++ estructuras-de-control/estructuras-selectivas/ejercicio_05.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 /**
- * Leer dis números positivos. si son diferentes se debe calcular la diferencia
+ * Leer dos números positivos. si son diferentes se debe calcular la diferencia
  * y si son iguales la suma.
  */
 
 class ejercicio_05 {
 
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int primerNumero = 0;
+    int segundoNumero = 0;
+
+    System.out.print("\nIngrese PRIMER número positivo = ");
+    primerNumero = sc.nextInt();
+
+    System.out.print("Ingrese SEGUNDO número positivo = ");
+    segundoNumero = sc.nextInt();
+
+    if (primerNumero == segundoNumero) {
+      int suma = primerNumero + segundoNumero;
+      System.out.println("\n1. " + primerNumero + " + " + segundoNumero + " = " + suma);
+    } else {
+      int resta = primerNumero - segundoNumero;
+      System.out.println("\n1. " + primerNumero + " - " + segundoNumero + " = " + resta);
+    }
+
+    sc.close();
+  }
 }
