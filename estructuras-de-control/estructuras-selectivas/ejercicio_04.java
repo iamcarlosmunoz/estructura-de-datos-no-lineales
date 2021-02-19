fe74a08c4b842fb2690de0a585d705d9c580a04d
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-18 21:48:30 -0500

    add exercise 05 and 04 es

---
+++ estructuras-de-control/estructuras-selectivas/ejercicio_04.java
@@ -0,0 +1,10 @@
+/**
+ * Calcular el salario neto de un trabajador, su salario basico se calcula con
+ * base en el número de horas trabajadas, el precio por hora y las horas extra
+ * (sí las tiene). El trabajador tine un descuento del 3% para ahorro si su
+ * salario basico es menor o igual a $350.000. En caso contrario el descuento es
+ * del 5%.
+ */
+public class ejercicio_04 {
+
+}
---
+++ estructuras-de-control/estructuras-selectivas/ejercicio_05.java
@@ -0,0 +1,8 @@
+/**
+ * Leer dis números positivos. si son diferentes se debe calcular la diferencia
+ * y si son iguales la suma.
+ */
+
+class ejercicio_05 {
+
+}
