6600da1d4fe2707b963c4a197c08b5784c579444
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-21 13:01:54 -0500

    completed exercise_04 es

--- estructuras-de-control/estructuras-selectivas/ejercicio_04.java
+++ estructuras-de-control/estructuras-selectivas/ejercicio_04.java
@@ -7,11 +7,44 @@ import java.util.Scanner;
  * salario basico es menor o igual a $350.000. En caso contrario el descuento es
  * del 5%.
  */
+
 public class ejercicio_04 {
 
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
 
+    // Entrada
+    float precioHora = 0f;
+    int horas = 0;
+    int horasExtra = 0;
+
+    // Salida
+    float salarioBasico = 0f;
+    float descuento = 0f;
+    float salarioNeto = 0f;
+
+    System.out.print("\nIngrese precio por hora: ");
+    precioHora = sc.nextFloat();
+
+    System.out.print("Ingrese número de horas: ");
+    horas = sc.nextInt();
+
+    System.out.print("Ingrese número de horas extras: ");
+    horasExtra = sc.nextInt();
+
+    salarioBasico = (horas + horasExtra) * precioHora;
+
+    if (salarioBasico <= 350000) {
+      descuento = salarioBasico * 3 / 100;
+    } else {
+      descuento = salarioBasico * 5 / 100;
+    }
+
+    salarioNeto = salarioBasico - descuento;
+
+    System.out.println("Salario Neto es de = " + salarioNeto);
+
+    sc.close();
   }
 }
