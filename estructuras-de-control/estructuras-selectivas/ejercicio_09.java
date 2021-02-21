8c9e25d3a9a361234d04df48aa08fd2dc7724d4b
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-21 13:37:57 -0500

    completed exercise_09 es

---
+++ estructuras-de-control/estructuras-selectivas/ejercicio_09.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Leer la nota final de un estudiante (entre cero y cinco) y determinar si gana
+ * o pierde la materia.
+ */
+
+public class ejercicio_09 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int nota = 0;
+
+    System.out.println("\nIngrese la nota final = ");
+    nota = sc.nextInt();
+
+    if (nota < 3 && nota >= 0) {
+      System.out.println("\nPerdio la materia.");
+    } else if (nota >= 3 && nota <= 5) {
+      System.out.println("\nGano la materia.");
+    } else {
+      System.out.println("\nNota ingresada no valida.");
+    }
+
+    sc.close();
+  }
+}
