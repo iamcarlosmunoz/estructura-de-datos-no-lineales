304c90daef58c0f051d2829bc66833e2a62613a0
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-22 09:06:14 -0500

    added exercise_12 es

---
+++ estructuras-de-control/estructuras-selectivas/ejercicio_12.java
@@ -0,0 +1,9 @@
+/**
+ * Un vendedor desea conocer su comision total al final del día. Suponga que en
+ * el almacen solo hay articulos de tres precios con la siguiente comision:
+ * $100.000 (comision 3%) $300.000 (comision 5%) $500.000 (comision 8%)
+ */
+
+public class ejercicio_12 {
+
+}
