6839703b67ffd652168df757cbdc8c000ef4d3cb
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-23 11:55:06 -0500

    completed exercise_12 es

--- estructuras-de-control/estructuras-selectivas/ejercicio_12.java
+++ estructuras-de-control/estructuras-selectivas/ejercicio_12.java
@@ -12,5 +12,29 @@ public class ejercicio_12 {
 
     Scanner sc = new Scanner(System.in);
 
+    // Variables Entrada
+    int articuloTipo1 = 0; // $100.000 (comision 3%)
+    int articuloTipo2 = 0; // $300.000 (comision 5%)
+    int articuloTipo3 = 0; // $500.000 (comision 8%)
+
+    // Variables Salida
+    float comision = 0f;
+
+    System.out.print("\nIngrese articulos vendidos con valor ($100.000): ");
+    articuloTipo1 = sc.nextInt();
+
+    System.out.print("Ingrese articulos vendidos con valor ($300.000): ");
+    articuloTipo2 = sc.nextInt();
+
+    System.out.print("Ingrese articulos vendidos con valor ($500.000): ");
+    articuloTipo3 = sc.nextInt();
+
+    if (articuloTipo1 != 0 && articuloTipo2 != 0 && articuloTipo3 != 0) {
+      comision = ((articuloTipo1 * 100000) * 3 / 100) + ((articuloTipo2 * 100000) * 3 / 100)
+          + ((articuloTipo3 * 100000) * 3 / 100);
+      System.out.println("\nComision: " + comision);
+    } else {
+      System.out.println("\nSin comisiones");
+    }
   }
 }
