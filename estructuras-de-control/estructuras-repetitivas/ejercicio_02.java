a6254f96fc719edc773e54710d451c0c4a98f7c3
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-02-26 21:53:02 -0500

    added exercise_02 er

---
+++ estructuras-de-control/estructuras-repetitivas/ejercicio_02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Leer un número entero y calcular la sumatoria hasta dicho número. (∑ = 1 + 2
+ * + 3 + … + n)
+ */
+
+public class ejercicio_02 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    sc.close();
+
+  }
+}
