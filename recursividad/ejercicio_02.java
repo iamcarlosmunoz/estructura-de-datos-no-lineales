7c350a91bdd7b8d71f5d0208720dc43b36368931
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-03-11 23:31:46 -0500

    completed exercise_03 and update exercise_02 rc

--- recursividad/ejercicio_02.java
+++ recursividad/ejercicio_02.java
@@ -1,5 +1,9 @@
 package recursividad;
 
+/**
+ * Leer un número entero y calcular la sumatoria hasta el numero leído.
+ */
+
 public class ejercicio_02 {
 
 }
---
+++ recursividad/ejercicio_03.java
@@ -0,0 +1,9 @@
+package recursividad;
+
+/**
+ * Leer un valor entero y calcular la sumatoria 1 + 1/2 +1/3+ … 1/n.
+ */
+
+public class ejercicio_03 {
+
+}
