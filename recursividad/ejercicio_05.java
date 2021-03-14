01fde6ce6e8e447f83b7a163d72f8c8b808a621c
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-03-13 21:18:07 -0500

    add exercise_05 rc

---
+++ recursividad/ejercicio_05.java
@@ -0,0 +1,11 @@
+package recursividad;
+
+/**
+ * Leer un número entero e invertirlo. Ejemplo: Entrada: 123 Salida: 321.
+ * Imprima el numero invertido en el programa principal(Se debe invertir en la
+ * función y devolver al programa principal)
+ */
+
+public class ejercicio_05 {
+
+}
