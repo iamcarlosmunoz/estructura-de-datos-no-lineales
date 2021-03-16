57ecf358ee49f017455ca0b93e605066036b0bd4
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-03-15 22:25:42 -0500

    update exercise_05 rc

--- recursividad/ejercicio_05.java
+++ recursividad/ejercicio_05.java
@@ -1,5 +1,7 @@
 package recursividad;
 
+import java.util.Scanner;
+
 /**
  * Leer un número entero e invertirlo. Ejemplo: Entrada: 123 Salida: 321.
  * Imprima el numero invertido en el programa principal(Se debe invertir en la
@@ -8,4 +10,24 @@ package recursividad;
 
 public class ejercicio_05 {
 
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int number;
+
+    System.out.print("\nIngrese el valor: ");
+    number = sc.nextInt();
+
+    sc.close();
+
+    System.out.print("\nResultado = " + reverseNumber(number, 0) + "\n");
+
+  }
+
+  public static int reverseNumber(int number, int i) {
+    if (number == 0)
+      return i;
+    else
+      return reverseNumber(number / 10, i * 10 + number % 10);
+  }
 }
